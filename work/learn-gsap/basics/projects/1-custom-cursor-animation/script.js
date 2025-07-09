2b3f1c8b2668e22c87fe33d01739620caf6b99ab
KayVeeZ <kshitijvashisth@gmail.com>
2025-07-09 22:41:54 +0100

    no idea waht

--- work/learn-gsap/basics/projects/1-custom-cursor-animation/script.js
+++ work/learn-gsap/basics/projects/1-custom-cursor-animation/script.js
@@ -2,26 +2,33 @@ var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
 var imageDiv = document.querySelector("#image");
 
+// Initially hide cursor
+cursor.style.opacity = 0;
+
 main.addEventListener("mousemove", function (dets) {
     gsap.to(cursor, {
         x:dets.x,
         y:dets.y,
-        duration:1,
-        ease:"back.out"
+        duration:0.1,
+        // ease:"back.out"
     });
 });
 
 imageDiv.addEventListener("mouseenter", function () {
     cursor.innerHTML="view more";
     gsap.to(cursor, {
+        opacity: 1,
         scale:4,
         backgroundColor: "#ffffff8a"
     });
+    main.classList.add("hide-native-cursor");  // Hide system cursor
 });
 imageDiv.addEventListener("mouseleave", function () {
     cursor.innerHTML="";
     gsap.to(cursor, {
         scale:1,
+        opacity:0,
         backgroundColor: "#fff"
     });
+    main.classList.remove("hide-native-cursor");  // Show system cursor again
 });
